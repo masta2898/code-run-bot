07790cf26f49e182cfa3a13b2a60e59cb4c25451
Master <kulikov.paul99@gmail.com>
2019-01-06 02:27:04 +0200

    implemented python highlighter

--- src/highlighters/python_highlighter.py
+++ src/highlighters/python_highlighter.py
@@ -1,9 +1,16 @@
 from src.highlighters.highlighter import Highlighter
 
+from pygments import highlight
+from pygments.lexers.python import PythonLexer
+from pygments.formatters.html import HtmlFormatter
+
 
 class PythonHighlighter(Highlighter):
     def __init__(self):
         pass
 
     def highlight(self, code: str) -> str:
-        return f"{code}\n highlighted!"
+        formatter = HtmlFormatter()
+        styles = f"<style>\n{formatter.get_style_defs('.highlight')}\n</style>"
+        code = highlight(code, PythonLexer(), formatter)
+        return f"{styles}\n{code}"
